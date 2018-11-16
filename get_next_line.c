7cac958928283b4812aa72cc36accb5042e42fea
bafraiki <bafraiki@student.42.fr>
2018-11-16 22:55:18 +0100

    done

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,19 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 12:04:12 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 12:04:15 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/16 14:43:00 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+
+
+
+
+
+
+int	get_next_line(const int fd,char **line)
+{
+	static t_
+
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,24 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 12:04:28 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/16 12:04:30 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/16 14:42:07 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+
+int	get_next_line(const int fd,char **line);
+
+typedef struct		s_fdline {
+
+	int			fd;
+	char		*rest;
+	t_fdline	*next;
+
+}					t_fdline;
+
+#endif
