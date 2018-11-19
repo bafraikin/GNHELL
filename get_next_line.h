266da45d26826d74fcf2464e61521046ee5f2c59
bafraiki <bafraiki@student.42.fr>
2018-11-19 13:46:30 +0100

    fast forward

--- get_next_line.h
+++ get_next_line.h
@@ -16,14 +16,38 @@
 
 # define BUFF_SIZE 32
 
+#include <stdlib.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
 int	get_next_line(const int fd,char **line);
 
-typedef struct		s_fdline {
+typedef	struct		s_list
+{
+  void			*content;
+  size_t			content_size;
+  struct s_list	*next;
+}					t_list;
 
-	int			fd;
-	char		*rest;
-	t_fdline	*next;
+typedef struct          s_fdlin
+{
+  char          *l_line;
+  char          *l_rest;
+  int           size_line;
+  int           size_rest;
+  int           mode;
+  int           fd;
+}                       t_fdlin;
 
-}					t_fdline;
+typedef struct        s_read
+{
+  int ret;
+  int total;
+  int mode;
+  char *ptr_l;
+  char *is_read;
+  char *tmp;
+}                     t_read;
 
 #endif
