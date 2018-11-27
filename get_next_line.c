91a9ad02244eb2740a1020c7f43736e4c1259593
bafraiki <bafraiki@student.42.fr>
2018-11-27 15:37:49 +0100

    still 32 bytes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/16 12:04:12 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/11/26 14:05:29 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/11/27 15:37:10 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ static void		ft_dellist(void *t_content, size_t size)
 {
 	t_fd *to_f;
 
-	ft_bzero(t_content, size);
 	to_f = (t_fd*)t_content;
 	if (to_f->l_rest)
 		free(to_f->l_rest);
@@ -59,21 +58,17 @@ static int		ft_gest_list(t_list **begin, const int fd, t_read *l_read,
 		return (0);
 	else if (!new->l_line)
 		return (-1);
-	else if (l_read->mod == 0)
-	{
+	if (l_read->mod == 0)
 		*line = ft_strsub(l_read->is_r, 0, new->size_line);
-		ft_list_remove_if(begin, (void*)&fd, ft_cmplist, ft_dellist);
-	}
-	free(l_read->is_r);
 	return (1);
 }
 
-static t_read	*ft_recup_fd(const int fd, t_read *t_r, int mode)
+static	t_read	*ft_recup_fd(const int fd, t_read *t_r, int mode)
 {
 	t_r->tot = (mode) ? ((t_fd*)(t_r->old))->size_rest : 0;
 	t_r->is_r = (mode) ? ((t_fd*)(t_r->old))->l_rest : (char*)malloc(1);
-	if (t_r->is_r == NULL || t_r->buff == NULL)
-		return (NULL);
+	if (t_r->is_r == NULL)
+		t_r->is_r = (char*)malloc(1);
 	if ((t_r->pl = ft_memchr(t_r->is_r, '\n', t_r->tot)) != NULL)
 		t_r->mod = &t_r->is_r[t_r->tot - 1] - t_r->pl + 1;
 	while (t_r->mod == 0 && ((t_r->r = read(fd, t_r->buff, BUFF_SIZE)) > 0))
@@ -81,8 +76,8 @@ static t_read	*ft_recup_fd(const int fd, t_read *t_r, int mode)
 		if ((t_r->tmp = (char*)malloc(sizeof(char) * t_r->tot)) == NULL)
 			return (NULL);
 		ft_memcpy(t_r->tmp, t_r->is_r, t_r->tot);
-		free(t_r->is_r);
 		t_r->tot += t_r->r;
+		free(t_r->is_r);
 		if ((t_r->is_r = (char*)malloc(sizeof(char) * t_r->tot + 1)) == NULL)
 			return (NULL);
 		ft_memcpy(t_r->is_r, t_r->tmp, t_r->tot - t_r->r);
@@ -99,37 +94,26 @@ static t_read	*ft_recup_fd(const int fd, t_read *t_r, int mode)
 int				get_next_line(const int fd, char **line)
 {
 	static t_list	*begin = NULL;
-	t_read			*l_read;
+	t_read			l_read;
+	t_read			*ptr;
 	int				tmp;
 
-	l_read = (t_read*)malloc(sizeof(t_read));
-	(l_read) ? ft_bzero(l_read, sizeof(t_read)) : 0;
-	(l_read) ? l_read->buff = (char*)malloc(sizeof(char) * BUFF_SIZE) : 0;
-	if (line && fd >= 0 && l_read)
+	ptr = &l_read;
+	ft_bzero(&l_read, sizeof(t_read));
+	l_read.buff = (char*)malloc(sizeof(char) * BUFF_SIZE);
+	if (line && fd >= 0)
 	{
-		if ((l_read->old = ft_list_find(begin, (void*)&fd, ft_cmplist)) == NULL)
-			l_read = ft_recup_fd(fd, l_read, 0);
-		else if ((l_read->old = ((t_list*)(l_read->old))->content) != NULL)
-			l_read = ft_recup_fd(fd, l_read, 1);
+		if ((l_read.old = ft_list_find(begin, (void*)&fd, ft_cmplist)) == NULL)
+			ptr = ft_recup_fd(fd, &l_read, 0);
+		else if ((l_read.old = ((t_list*)(l_read.old))->content) != NULL)
+			ptr = ft_recup_fd(fd, &l_read, 1);
 	}
-	if (!(line && fd >= 0 && l_read))
+	if (!(line && fd >= 0 && ptr) || l_read.r == -1)
 		return (-1);
-	tmp = ft_gest_list(&begin, fd, l_read, line);
-	if (!line)
-		free(l_read);
-	if (tmp == 0 || tmp == -1)
-		ft_list_remove_if(&begin, (void*)&fd, ft_cmplist, ft_dellist);
-	return (tmp);
-}
-int		main(void)
-{
-	int fd = open("coucou", O_RDONLY);
-	int ret = 0;
-	char *c;
+	tmp = ft_gest_list(&begin, fd, ptr, line);
+	free(l_read.is_r);
 
-	while((ret = get_next_line(4, &c)))
-		printf("(%d) %s\n", ret, c);
-	printf("(%d) %s\n", ret, c);
-	printf("(%d) %s\n", ret, c);
-	return 0;
+	if ((l_read.mod == 0 && tmp == 1) || (!line && (tmp == 0 || tmp == -1)))
+			ft_list_remove_if(&begin, (void*)&fd, ft_cmplist, ft_dellist);
+	return (tmp);
 }
