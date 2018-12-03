842f408afd14ac7753d09e187a154fe825f2c097
bafraiki <bafraiki@student.42.fr>
2018-12-03 11:43:06 +0100

    libft modified

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: bafraiki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 14:33:39 by bafraiki          #+#    #+#             */
-/*   Updated: 2018/12/03 11:04:27 by bafraiki         ###   ########.fr       */
+/*   Updated: 2018/12/03 11:40:46 by bafraiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list				*ft_lstadd(void *content, size_t content_size);
+t_list				*ft_lst_add_cont(void *content, size_t content_size);
+void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_list_add_back(t_list **begin_list, void *content,
 		size_t content_size);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
